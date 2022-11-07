3148ab112f6fe59e94d0bfa23e35fe40d3d7f030
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-11-07 13:48:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access key = "AKIA5QKJ7BKOIMGXJFS2"
-  secret key = "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"
+  access_key = "AKIA5QKJ7BKOIMGXJFS2"
+  secret_key = "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"
 }
