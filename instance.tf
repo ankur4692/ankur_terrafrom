dd896c830ba7409b573a21aa40af9cc03b60ed88
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-11-07 13:58:33 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 # creating instance
 resource "aws_instance" "web" {
   ami           = "ami-09d3b3274b6c5d4aa"
-  instance_type = "var.Instance_type"
+  instance_type = var.Instance_type
   key_name   = "${aws_key_pair.deployer.key_name}"
   tags = {
     Name = "HelloWorld"
