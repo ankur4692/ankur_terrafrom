31bfc6bb26b22332472a4bb1f6a7d6313c7b7433
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-11-07 14:08:35 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -9,15 +9,3 @@ resource "aws_instance" "web" {
   }
 }
 
-# creating key pair
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrzmhKjEIFPedxVDdBS6GW6GT88FRHESjAqugAB4ywlznw/qcF7Vf8TPF/CxgU7n4qUr4p91fLDqFbJnLdjZpZw2jEcS9LW3cFwR23719Le1v6uACGldxpECZ1+WDMNppJosSOMBj9iNSinn5NuDnxqhnA0hKJYnlbCr8ZTreWUgyfZDo4dNE6013ArIXIyry6wiR3SqYv+e6QhjsBfv8pDiOnNapKRtmsdL3VRWrkC83NFqWnI0Fn1a06t0wGVvfsYnw0a5uxLobdYoqGe+X52mOTNmMUPHcxBgQRdl9UkTwupS/tHonhI2X3849lszPoJ3Icgss7gT8JUDkWvxuj root@ip-172-31-29-51.ec2.internal"
-  #public_key = file("${path.module}/id_rsa.pub")       
-}
-
-output "keyname" {
-	value = "${aws_key_pair.deployer.key_name}"
-}
-
-
