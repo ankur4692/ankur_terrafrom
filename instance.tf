6163081d3f57dfcb3279c7525fb45dd92279ee09
Ankur <47654709+ankur4692@users.noreply.github.com>
2022-11-07 13:43:23 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@
 # creating instance
 resource "aws_instance" "web" {
   ami           = "ami-09d3b3274b6c5d4aa"
-  instance_type = "t2.micro"
+  instance_type = "var.Instance_type"
   key_name   = "${aws_key_pair.deployer.key_name}"
   security_groups = "${aws_security_group.allow_tls.id}]"
   tags = {
